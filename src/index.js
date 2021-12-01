548dba4eb2befda6c0101fa35abf85c18c86c5e9
twomeetings <2326220@163.com>
2021-12-01 15:07:20 +0800

    finish: step 3

--- src/index.js
+++ src/index.js
@@ -38,6 +38,21 @@ function render(element, container) {
   container.appendChild(dom)
 }
 
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork) {}
+
 const Jeact = {
   createElement,
   render,
