b4bfeea16885e853a40667b5266990edcd65f5cd
twomeetings <2326220@163.com>
2021-12-01 15:29:56 +0800

    finish: step 4

--- src/index.js
+++ src/index.js
@@ -20,23 +20,50 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
-  // const dom = document.createElement(element.type)
+function createDom(fiber) {
   const dom =
-    element.type === 'TEXT_ELEMENT'
+    fiber.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type)
+      : document.createElement(fiber.type)
 
   const isProperty = (key) => key !== 'children'
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
 
-  element.props.children.forEach((child) => render(child, dom))
-  container.appendChild(dom)
+  return dom
 }
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+}
+
+let nextUnitOfWork = null
+
+// function render(element, container) {
+//   // const dom = document.createElement(element.type)
+//   const dom =
+//     element.type === 'TEXT_ELEMENT'
+//       ? document.createTextNode('')
+//       : document.createElement(element.type)
+
+//   const isProperty = (key) => key !== 'children'
+//   Object.keys(element.props)
+//     .filter(isProperty)
+//     .forEach((name) => {
+//       dom[name] = element.props[name]
+//     })
+
+//   element.props.children.forEach((child) => render(child, dom))
+//   container.appendChild(dom)
+// }
 
 function workLoop(deadline) {
   let shouldYield = false
@@ -51,7 +78,52 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {}
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+
+    index++
+  }
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+
+    nextFiber = nextFiber.parent
+  }
+}
 
 const Jeact = {
   createElement,
