48951ebb735fa7aca941548af80f675e1671a7b3
xulianghui <xulianghui@sfmail.sf-express.com>
2021-12-02 10:29:24 +0800

    finish: step 6

--- src/index.js
+++ src/index.js
@@ -14,11 +14,16 @@ function createTextElement(text) {
   return {
     type: 'TEXT_ELEMENT',
     props: {
-      nodeValue: text + '-J',
+      nodeValue: text,
       children: [],
     },
   }
 }
+
+const isEvent = (key) => key.startsWith('on')
+const isProperty = (key) => key !== 'children' && !isEvent(key)
+const isNew = (prev, next) => (key) => prev[key] !== next[key]
+const isGone = (prev, next) => (key) => !(key in next)
 
 function createDom(fiber) {
   const dom =
@@ -26,19 +31,63 @@ function createDom(fiber) {
       ? document.createTextNode('')
       : document.createElement(fiber.type)
 
-  const isProperty = (key) => key !== 'children'
   Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
       dom[name] = fiber.props[name]
     })
 
+  Object.keys(fiber.props)
+    .filter(isEvent)
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, fiber.props[name])
+    })
+
   return dom
+}
+
+function updateDom(dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)[key])
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeEventListerner(eventType, prevProps[name])
+    })
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = ''
+    })
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name]
+    })
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, nextProps[name])
+    })
 }
 
 function commitRoot() {
   // TODO add nodes to dom
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
@@ -48,7 +97,14 @@ function commitWork(fiber) {
   }
 
   const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
+  // domParent.appendChild(fiber.dom)
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom)
+  }
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
@@ -60,14 +116,17 @@ function render(element, container) {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   }
 
+  deletions = []
   nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let currentRoot = null
 let wipRoot = null
-
+let deletions = null
 // function render(element, container) {
 //   // const dom = document.createElement(element.type)
 //   const dom =
@@ -103,6 +162,73 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop)
 
+function reconcileChildren(wipFiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+
+  let prevSibling = null
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index]
+    let newFiber = null
+
+    const sameType = oldFiber && element && element.type === oldFiber.type
+
+    if (sameType) {
+      // TODO update the node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE',
+      }
+    }
+
+    if (element && !sameType) {
+      // TODO add this node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: 'PLACEMENT',
+      }
+    }
+
+    if (oldFiber && !sameType) {
+      // TODO delete the oldFiber's node
+      oldFiber.effectTag = 'DELETION'
+      deletions.push(oldFiber)
+    }
+
+    // TODO compare oldFiber to element
+
+    // const newFiber = {
+    //   type: element.type,
+    //   props: element.props,
+    //   parent: fiber,
+    //   dom: null,
+    // }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+
+    index++
+  }
+}
+
 function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
@@ -113,28 +239,7 @@ function performUnitOfWork(fiber) {
   // }
 
   const elements = fiber.props.children
-  let index = 0
-  let prevSibling = null
-  while (index < elements.length) {
-    const element = elements[index]
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    }
-
-    if (index === 0) {
-      fiber.child = newFiber
-    } else {
-      prevSibling.sibling = newFiber
-    }
-
-    prevSibling = newFiber
-
-    index++
-  }
+  reconcileChildren(fiber, elements)
 
   if (fiber.child) {
     return fiber.child
@@ -163,13 +268,30 @@ const Jeact = {
 // )
 
 // 下面的标记会触发babel用我们自己定义的方法处理jsx代码
-/** @jsx Jeact.createElement */
-const element = (
-  <div style="background: salmon">
-    <h1>Hello World</h1>
-    <h2 style="text-align: right">from Jeact</h2>
-  </div>
-)
+// const element = (
+//   <div style="background: salmon">
+//     <h1>Hello World</h1>
+//     <h2 style="text-align: right">from Jeact</h2>
+//   </div>
+// )
 
+/** @jsx Jeact.createElement */
 const container = document.getElementById('root')
-Jeact.render(element, container)
+const updateValue = (e) => {
+  console.log('=== e.target.value:', e.target.value)
+  rerender(e.target.value)
+}
+
+const rerender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value}></input>
+      <h2>Hello {value}</h2>
+    </div>
+  )
+
+  Jeact.render(element, container)
+}
+
+// Jeact.render(element, container)
+rerender('World')
